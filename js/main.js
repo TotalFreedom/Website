a52a1290fcc08b5b0174df64a2b599177b2db199
aggelosQQ <jouflis1@hotmail.com>
2016-10-24 15:52:27 +0300

    Added page preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", function()  {
+    var load_screen = document.getElementById("load_screen");
+    document.body.removeChild(load_screen);
+});
+
 $(document).ready(function() {
     $(".button-collapse").sideNav({
         closeOnClick: true
