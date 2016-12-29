f98b19fd9da3e7d07d1751308573fa4eb14d1339
aggelosQQ <jouflis1@hotmail.com>
2016-12-29 18:50:14 +0200

    New font, style modifications & typed.js

--- js/main.js
+++ js/main.js
@@ -43,6 +43,12 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    $('.brand-logo').typed({
+        strings: ['<small class="hide-on-med-and-down">play.</small>TotalFreedom<small class="hide-on-med-and-down">.me</small>'],
+        typeSpeed: 30
+    });
+
+
     (new Date).getFullYear();
     $("#curYear").text((new Date).getFullYear())
 });
