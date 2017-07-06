75b06afab059a9045e346d27f82f45c626088e4a
aggelosQQ <jouflis1@hotmail.com>
2017-07-06 15:42:49 +0300

    Change of the License
    TotalFreedom's website is now licensed under the Apache License.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+/*
+
+  Copyright 2017 Aggelos Sarris and contributors
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+      
+*/
+
 $(document).ready(function() {
     $(".button-collapse").sideNav({
         closeOnClick: true
